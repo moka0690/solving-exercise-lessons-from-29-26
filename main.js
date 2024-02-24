44f2c1c0a5d7edae8b85d8bdc94de87c3c1886db
moka0690 <madradz39@gmail.com>
2024-02-24 20:35:15 +0100

    solveSolving exercise 1

---
+++ main.js
@@ -0,0 +1,16 @@
+// Examples
+console.log(100_000); // 100000
+console.log(100000); // 100000
+console.log(5e4 + 5e4); // 100000
+
+// Your Solutions
+console.log( 100*1000 ); // 100000
+console.log(10**5); // 100000
+console.log(10*10*10*10*10); // 100000
+console.log(100000.00); // 100000
+console.log(1000000/10); // 100000
+console.log(+("100000")); // 100000
+console.log(-"-100000"); // 100000
+console.log(-"-100000"/true); // 100000
+console.log(+"100000"/true); // 100000
+console.log(10000+[(true+false)]-true); // 100000
