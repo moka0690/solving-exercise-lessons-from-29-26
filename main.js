ea4a8c4d43637ef72862c46a79fb8a18bcfe3852
moka0690 <madradz39@gmail.com>
2024-02-26 19:44:31 +0100

    solveSolving exercise 6

--- main.js
+++ main.js
@@ -30,3 +30,14 @@ console.log(parseFloat(myVar).toFixed(2)); // 100.57
 let num = 10;
 
 console.log((Number.isInteger(num))+(true)); // 2
+
+let flt = 10.4;
+
+console.log(flt.toFixed()); // 10
+console.log(parseInt(flt)); // 10
+console.log( Math.round(flt)); // 10
+console.log(Math.floor(flt)); // 10
+console.log(Math.trunc(flt)); // 10
+
+
+
